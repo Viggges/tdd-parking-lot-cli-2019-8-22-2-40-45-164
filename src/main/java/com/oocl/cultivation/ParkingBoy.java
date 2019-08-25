a57494cad4b9e93c6c6763e344aa2b3645211469
lw <623656827@qq.com>
2019-08-25 21:34:50 +0800

    done

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -2,24 +2,38 @@ package com.oocl.cultivation;
 
 public class ParkingBoy {
 
-    private final ParkingLot parkingLot;
-    private String lastErrorMessage;
+	private final ParkingLot parkingLot;
+	private String lastErrorMessage;
 
-    public ParkingBoy(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
-    }
+	public ParkingBoy(ParkingLot parkingLot) {
+		this.parkingLot = parkingLot;
+	}
 
-    public ParkingTicket park(Car car) {
-        // TODO: Please implement the method
-        throw new RuntimeException("Not implemented");
-    }
+	public ParkingTicket park(Car car) {
+		// TODO: Please implement the method
+		if (parkingLot.getAvailableParkingPosition() >= 0) {
+			lastErrorMessage = "The parking lot is full.";
+			return null;
+		}
+		ParkingTicket parkingTicket = parkingLot.fetchTicket(car);
+		lastErrorMessage = null;
+		return parkingTicket;
+	}
 
-    public Car fetch(ParkingTicket ticket) {
-        // TODO: Please implement the method
-        throw new RuntimeException("Not implemented");
-    }
+	public Car fetch(ParkingTicket ticket) {
+		// TODO: Please implement the method
+		if (ticket == null) {
+			lastErrorMessage = "Please provide your parking ticket.";
+			return null;
+		}
+		Car car = parkingLot.fetchCar(ticket);
+		if (car == null) {
+			lastErrorMessage = "Unrecognized parking ticket.";
+		}
+		return car;
+	}
 
-    public String getLastErrorMessage() {
-        return lastErrorMessage;
-    }
+	public String getLastErrorMessage() {
+		return lastErrorMessage;
+	}
 }
--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -4,18 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
-    private final int capacity;
-    private Map<ParkingTicket, Car> cars = new HashMap<>();
+	private final int capacity;
+	private Map<ParkingTicket, Car> cars = new HashMap<>();
 
-    public ParkingLot() {
-        this(10);
-    }
+	public ParkingLot() {
+		this(10);
+	}
 
-    public ParkingLot(int capacity) {
-        this.capacity = capacity;
-    }
+	public ParkingLot(int capacity) {
+		this.capacity = capacity;
+	}
 
-    public int getAvailableParkingPosition() {
-        return cars.size() - capacity;
-    }
+	public int getAvailableParkingPosition() {
+		return cars.size() - capacity;
+	}
+
+	public ParkingTicket fetchTicket(Car car) {
+		ParkingTicket parkingTicket = new ParkingTicket();
+		cars.put(parkingTicket, car);
+		return parkingTicket;
+	}
+
+	public Car fetchCar(ParkingTicket parkingTicket) {
+		Car car = cars.get(parkingTicket);
+		cars.remove(parkingTicket);
+		return car;
+	}
+
 }
